f713de05ae51a931e4349b1aa1ff583e5fad986a
Niels <nbijl@petersplanet.nl>
2021-04-29 10:56:18 +0200

    Initial commit

---
+++ mapper.py
@@ -0,0 +1,22 @@
+import sys
+
+string = sys.stdin.read()
+
+allowed_chars = 'abcdefghijklmnopqrstuvwxyz '
+
+for index in range(len(string)):
+    if index < len(string) - 1:
+        char0 = string[index]
+        if char0 not in allowed_chars:
+            char0 = '_'
+        char1 = string[index + 1]
+        if char1 not in allowed_chars:
+            char1 = '_'
+
+        print('%s\t%s' % (char0 + char1, 1))
+
+
+
+"""
+RUN: mapper.py < text.txt > mapoutput.txt
+"""
