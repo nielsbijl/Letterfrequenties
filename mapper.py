86c64326c9d5af3bf2f9a5bcee32d67448f3d4b6
Niels <nbijl@petersplanet.nl>
2021-04-30 10:33:36 +0200

    code improvement

--- mapper.py
+++ mapper.py
@@ -8,18 +8,16 @@ if __name__ == "__main__":
 
     chars = list(map(lambda x: x if x in allowed_chars else '_', text))
 
-    for index in range(len(chars)):
-        if index < len(chars) - 1:
-            char0 = chars[index]
-            char1 = chars[index + 1]
-            print('%s\t%s' % (char0 + char1, 1))
+    for index in range(len(chars) - 1):
+        char0 = chars[index]
+        char1 = chars[index + 1]
+        print('%s\t%s' % (char0 + char1, 1))
 
 
 
 """
-RUN: mapper.py < text.txt > mapoutput.txt
+RUN: mapper.py < text.txt
 
 run mapper.py with text.txt as stdin and write stdout to mapoutput.txt
-
-RUN ALL: mapper.py < text.txt | reduce.py
+RUN: mapper.py < text.txt > mapoutput.txt
 """
