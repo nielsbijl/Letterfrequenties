3c06386e26e6d1f00253de90352050badaede73a
Niels <nbijl@petersplanet.nl>
2021-04-30 10:42:37 +0200

    code improvement

--- reduce.py
+++ reduce.py
@@ -1,33 +1,30 @@
 import sys
 
-current_chars = None
-current_count = 0
-chars = None
 
-lines = sys.stdin.readlines()
-lines.sort()
+if __name__ == "__main__":
+    current_chars = None
+    current_count = 0
+    chars = None
 
-for line in lines:
+    lines = sys.stdin.readlines()
+    lines.sort()
 
-    chars, count = line.split('\t', 1)
-    count = int(count)
+    for line in lines:
+        chars, count = line.split('\t', 1)
+        count = int(count)
+
+        if current_chars == chars:
+            current_count += count
+        else:
+            if current_chars:
+                print('%s\t%s' % (current_chars, current_count))
+            current_count = count
+            current_chars = chars
 
     if current_chars == chars:
-        current_count += count
-    else:
-        if current_chars:
-            print('%s\t%s' % (current_chars, current_count))
-        current_count = count
-        current_chars = chars
-
-if current_chars == chars:
-    print('%s\t%s' % (current_chars, current_count))
+        print('%s\t%s' % (current_chars, current_count))
 
 
 """
-RUN: reduce.py < mapoutput.txt
-
-run reduce.py with mapoutput.txt as stdin
-
-RUN ALL: mapper.py < text.txt | reduce.py
+RUN: mapper.py < text.txt | reduce.py
 """
