a22c6ae6cdaaeaeb1f046e519c03ac7f9a70799d
Niels <nbijl@petersplanet.nl>
2021-05-10 18:25:04 +0200

    reduce to model matrix

--- floris/new/reduce_to_model.py
+++ floris/new/reduce_to_model.py
@@ -1,9 +1,11 @@
+import ast
 import sys
 import numpy as np
 
 matrices_raw = [line.rstrip('\n').split('\t')[1] for line in sys.stdin]
 matrices = []
 for matrix in matrices_raw:
-    matrices.append(np.frombuffer(matrix).reshape((28,28)))
-print(matrices[0])
+    matrix = ast.literal_eval(matrix)
+    matrices.append(np.frombuffer(matrix).reshape((28, 28)))
 matrix = np.average(matrices, axis=0)
+print(matrix)
